e57f48672ff6e037157b807d64967fb550851d2f
mehmetaksahin <mehmet.aksahin@testinium.com>
2026-01-19 17:23:40 +0300

    Hub Edit

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -69,7 +69,7 @@ public class HookImpl {
                 desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3000);
                 desiredCapabilities.setCapability("unicodeKeyboard", false);
                 //desiredCapabilities.setCapability("resetKeyboard", true);
-                URL url = new URL("http://127.0.0.1:4723/wd/hub");
+                URL url = new URL("http://127.0.0.1:4723/");
                 appiumDriver = new AndroidDriver(url, desiredCapabilities);
                 appiumDriver.hideKeyboard();
             } else {
@@ -86,7 +86,7 @@ public class HookImpl {
                 desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.3.1");
                 desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 300);
 
-                URL url = new URL("http://127.0.0.1:4723/wd/hub");
+                URL url = new URL("http://127.0.0.1:4723/");
                 appiumDriver = new IOSDriver(url, desiredCapabilities);
 
 
